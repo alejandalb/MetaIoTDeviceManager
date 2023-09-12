af0de977efa3c9678d1f16b547beb3ab7dae14ee
alejandroalbert998@gmail.com <alejandroalbert998@gmail.com>
2023-09-13 00:52:59 +0200

    Generación de proyecto Maven

---
+++ src/main/java/alalca3/metaIoT/MetaIoTApplication.java
@@ -0,0 +1,13 @@
+package alalca3.metaIoT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MetaIoTApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MetaIoTApplication.class, args);
+	}
+
+}
---
+++ src/test/java/alalca3/metaIoT/MetaIoTApplicationTests.java
@@ -0,0 +1,13 @@
+package alalca3.metaIoT;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MetaIoTApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
